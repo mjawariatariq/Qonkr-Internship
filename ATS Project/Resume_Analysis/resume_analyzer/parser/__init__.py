26bd3b1ccfa50b6e6d39364023bd5b514e1dd130
Jawaria Tariq <150448000+mjawariatariq@users.noreply.github.com>
2025-04-26 01:03:33 +0500

    Create __init__.py

---
+++ ATS Project/Resume_Analysis/resume_analyzer/parser/__init__.py
@@ -0,0 +1,10 @@
+# init file
+
+from .skill_extractor import extract_skills
+from utils.cleaner import clean_text
+
+# It tells Python that the folder it's in should be treated as a package 
+# (so you can import from it).
+
+# Jab kisi folder mein __init__.py hota hai, Python samajh jata hai ke yeh
+# folder ek proper package hai jisko hum import kar sakte hain.
